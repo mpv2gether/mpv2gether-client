b9fc4de1b60c1783a5bd7cb995dac07d993912c4
goeo_ <me@goeo.pink>
2017-02-27 20:34:30 +0300

    made websockets event base

--- websockets.js
+++ websockets.js
@@ -1,26 +1,38 @@
 const WebSocket = require('ws');
 
-function Messaging() {}
-Messaging.prototype.ws = null;
+function Messaging() {
+    this.ws = null;
+    this.events = {};
+}
 
 // msg = new Messaging();
 // msg.connect();
 
-Messaging.prototype.connect = function(){
-    this.ws = new WebSocket('ws://localhost:8765/ws');
-
-    this.ws.on("message", function incoming(json, flags) {
-        let data = JSON.parse(json)
-        handleMessage(data["type"], data["message"]);
-    });
+Messaging.prototype._handleMessage = function(type, message) {
+    if (this.events[type])
+        this.events[type](message);
+    else
+        console.log(type, message)
 }
 
 Messaging.prototype._send = function(message) {
     if (this.ws != null) {
         this.ws.send(JSON.stringify(message), function error(error) {
-            console.log(error);
+            if (error)
+                console.log(error);
         });
     }
+}
+
+Messaging.prototype.connect = function(){
+    this.ws = new WebSocket('ws://localhost:8765/ws');
+
+    var msgthis = this;
+
+    this.ws.on("message", function incoming(json, flags) {
+        let data = JSON.parse(json)
+        msgthis._handleMessage(data["type"], data["message"]);
+    });
 }
 
 Messaging.prototype.createSession = function(nick) {
@@ -59,45 +71,6 @@ Messaging.prototype.chatMessage = function(t_message) {
 
 module.exports = Messaging;
 
-function handleMessage(type, message) {
-    console.log(type);
-    switch(type) {
-        case "created_session":
-            createdSessionHandler(message);
-            break;
-        case "user_joined":
-            userJoinedHandler(message);
-            break;
-        case "user_left":
-            userLeftHandler(message);
-            break;
-        case "message":
-            messageHandler(message);
-            break;
-        case "error":
-        default:
-            errorHandler(message);
-            break;
-    }
-}
-
-// TODO: print useful things to chat :^)
-function createdSessionHandler(message) {
-    console.log("createdSessionHandler: " + JSON.stringify(message));
-}
-
-function userJoinedHandler(message) {
-    console.log("userJoinedHandler: " + JSON.stringify(message));
-}
-
-function userLeftHandler(message) {
-    console.log("userLeftHandler: " + JSON.stringify(message));
-}
-
-function messageHandler(message) {
-    console.log("messageHandler: " + JSON.stringify(message));
-}
-
-function errorHandler(message) {
-    console.log("errorHandler: " + JSON.stringify(message));
+Messaging.prototype.on = function(type, func) {
+    this.events[type] = func;
 }
